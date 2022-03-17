d02b0e79338e261b0de2c3edc460ecda8b5d1add
Alexander Le <alexanvl@uci.edu>
2022-03-17 05:33:24 -0700

    stuff
    Co-authored-by: Anthea Nguyen <anthean@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,13 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/ItemTrackerv2/",
   plugins: [react()],
   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
 });
+
+// export default defineConfig({
+//   base:"ItemTrackerv2",
+//   plugins: [react()],
+//   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
+// });
