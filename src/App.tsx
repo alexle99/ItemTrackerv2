defa224243d75e2937e0bf06fa22ef0baeb92f8c
Alexander Le <alexanvl@uci.edu>
2022-03-18 09:58:06 -0700

    works

--- src/App.tsx
+++ src/App.tsx
@@ -5,25 +5,21 @@ import { AccountSidebar, ItemDisplay, Navbar } from '@/components';
 import { Account, Item } from '@/types/account';
 
 const addItemList = ['item1', 'item2', 'item3', 'item4', 'item5'];
-
 const a1: Account = {
   id: uuid(),
   userName: 'john',
   items: [{ id: uuid(), name: 'bat' }],
 };
-
 const a2: Account = {
   id: uuid(),
   userName: 'bob',
   items: [{ id: uuid(), name: 'gun' }],
 };
-
 const a3: Account = {
   id: uuid(),
   userName: 'alex',
   items: [{ id: uuid(), name: 'orange peel' }],
 };
-
 const randomItems = (quantity: number): Item[] => {
   const items: Item[] = [];
   for (let i = 0; i < quantity; i++) {
@@ -32,15 +28,12 @@ const randomItems = (quantity: number): Item[] => {
   }
   return items;
 };
-
 export const App = () => {
   const [accounts, setAccounts] = useState<Account[]>([a1, a2, a3]);
   const [selectedAccount, setSelectedAccount] = useState<Account | undefined>();
-
   const handleSelected = (account: Account) => {
     setSelectedAccount(account);
   };
-
   const addRandomAccounts = () => {
     const account: Account = {
       id: uuid(),
@@ -49,7 +42,6 @@ export const App = () => {
     };
     setAccounts((prev) => [...prev, account]);
   };
-
   const addItemToItemList = (itemName: string) => {
     const item: Item = {
       id: uuid(),
@@ -63,7 +55,6 @@ export const App = () => {
       }
     }
   };
-
   const removeItemFromItemList = (itemToRemove: Item) => {
     for (const a in accounts) {
       if (accounts[a].userName === selectedAccount?.userName) {
@@ -75,7 +66,6 @@ export const App = () => {
       }
     }
   };
-
   return (
     <Box>
       <Navbar />
@@ -104,3 +94,7 @@ export const App = () => {
     </Box>
   );
 };
+
+// export const App = () => {
+//   return <h1>CHICKEN</h1>;
+// };
--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/ItemTrackerv2/",
   plugins: [react()],
   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
 });
-
-// export default defineConfig({
-//   base:"ItemTrackerv2",
-//   plugins: [react()],
-//   resolve: { alias: { '@': path.resolve(__dirname, 'src') } },
-// });
